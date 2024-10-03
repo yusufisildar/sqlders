aac842c389da4202db1ae8279b115c31caa24975
M. Yusuf Işıldar <103322233+yusufisildar@users.noreply.github.com>
2024-10-03 16:01:37 +0300

    Create odev3.sql

---
+++ sqlodev/odev3.sql
@@ -0,0 +1,15 @@
+--1)
+SELECT country FROM country
+WHERE country LIKE 'A%a';
+
+--2)
+SELECT country FROM country
+WHERE country LIKE '_____n';
+
+--3)
+SELECT title FROM film
+WHERE title ILIKE '%t%t%t%t%';
+
+--4)
+SELECT title, length, rental_rate FROM film
+WHERE title LIKE 'C%' AND length > 90 AND rental_rate = 2.99;
